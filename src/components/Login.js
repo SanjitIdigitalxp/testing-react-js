b7ae0cde9f52d7ffea18336652641e449b308e75
sanjit.maji@idigitalxp.com <sanjit.maji@idigitalxp.com>
2021-07-13 09:20:23 +0530

    add congrats component and test utils

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function Login() {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	useEffect(() => {
+		console.log('Email address :', email);
+		console.log('user password :', password);
+	}, [email, password]);
+
     return (
 			<div data-test='component-login'>
-                <div>Login</div>
-				<input data-test='login-input' placeholder='Email address'></input>
-				<input data-test='login-input' placeholder='password' type='password'></input>
-                <button>Login</button>
+				<div>Login</div>
+				<div>
+					<input
+						data-test='login-input'
+						placeholder='Email address'
+						data-test='inputEmail'
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}></input>
+						<span data-test="getEmail">{email}</span>
+				</div>
+				<div>
+					<input
+						data-test='getEmail'
+						placeholder='password'
+						type='password'
+						data-test='inputPassword'
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}></input>
+						{/* <span>{password}</span> */}
+				</div>
+				<div>
+					<button data-test='loginButton' onClick={(e)=>{
+						
+					}}>Login</button>
+				</div>
 			</div>
 		);
 }
--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -2,19 +2,65 @@ import React, { Component } from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 import Login from './Login';
-
+import { findByTestAttr } from '../../test/testUtils';
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+const setup = () => shallow(<Login />);
+
 test('render without error', () => {
-	const wrapper = shallow(<Login />);
-	const loginComponent = wrapper.find("[data-test='component-login']");
-    expect(loginComponent.length).toBe(1);
+	const wrapper = setup();
+	const loginComponent = findByTestAttr(wrapper, 'component-login');
+	expect(loginComponent.length).toBe(1);
 });
 
-test('render input field for email', () => {});
+test('render input field for email', () => {
+	const wrapper = setup();
+	const inputEmail = findByTestAttr(wrapper, 'inputEmail');
+	expect(inputEmail.length).toBe(1);
+});
 
-test('render input field for password', () => {});
+test('render input field for password', () => {
+	const wrapper = setup();
+	const inputPassword = findByTestAttr(wrapper, 'inputPassword');
+	expect(inputPassword.length).toBe(1);
+});
 
-test('render login button', () => {});
+test('render login button', () => {
+	const wrapper = setup();
+	const loginButton = findByTestAttr(wrapper, 'loginButton');
+	expect(loginButton.length).toBe(1);
+});
 
-test('on click login show alert with login data', () => {});
+test('getEmail initial input value', () => {
+	const wrapper = setup();
+	const getEmail = findByTestAttr(wrapper, 'getEmail').text();
+	expect(getEmail).toBe('');
+});
+
+test.skip('clicking on button give login to user', () => {
+	const wrapper = setup();
+	const inputEmail = findByTestAttr(wrapper, 'inputEmail').simulate('change', {
+		target: { value: 'sanjitmaji08@gmail.com' },
+	});
+	console.log(inputEmail.debug());
+	// inputEmail.simulate('change', {
+	// 	target: { value: 'sanjitmaji08@gmail.com' },
+	// });
+	const inputPassword = findByTestAttr(wrapper, 'inputPassword').simulate(
+		'change',
+		{ target: { value: 'Sanjit@09' } },
+	);
+	// inputPassword.simulate('change', {
+	// 	target: { value: 'Sanjit@09' },
+	// });
+
+	//find the button
+	const loginButton = findByTestAttr(wrapper, 'loginButton');
+
+	//click the button
+	loginButton.simulate('click');
+	//find the email address, and test that the we get users email id
+
+	expect(inputEmail).toBe('sanjitmaji08@gmail.com');
+	expect(inputPassword).toBe('Sanjit@09');
+});
