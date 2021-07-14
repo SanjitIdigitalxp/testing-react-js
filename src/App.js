2e7b5997d07b3ebb8ae52e48225ff4e6552079c8
sanjit.maji@idigitalxp.com <sanjit.maji@idigitalxp.com>
2021-07-14 16:00:34 +0530

    test for non zero guessed word

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+import GuessedWords from './components/GuessedWord';
 
 function App() {
-  return (
-    <div data-test='component-app'>
-     <Login/>
-    </div>
-  );
+	return (
+		<div data-test='component-app'>
+			<Login />
+			{/* <GuessedWords /> */}
+		</div>
+	);
 }
 
 export default App;
--- src/components/GuessWords.test.js
+++ src/components/GuessWords.test.js
@@ -17,18 +17,41 @@ test('dose not throw waring with expected props', () => {
 });
 
 describe('if there are no words gussed', () => {
-    let wrapper
-    beforeEach(()=>{
-         wrapper = setup({ guessedWords: [] });
-    })
+	let wrapper;
+	beforeEach(() => {
+		wrapper = setup({ guessedWords: [] });
+	});
 	test('renders without error', () => {
 		const component = findByTestAttr(wrapper, 'component-guessed-words');
 		expect(component.length).toBe(1);
 	});
-    test('renders instructions to guess a word', ()=>{
-        const instructions = findByTestAttr(wrapper, 'guess-instructions')
-        expect(instructions.text().length).not.toBe(0)
-    })
+	test('renders instructions to guess a word', () => {
+		const instructions = findByTestAttr(wrapper, 'guess-instructions');
+		expect(instructions.text().length).not.toBe(0);
+	});
 });
 
-describe('if there are words guessed', () => {});
+describe('if there are words guessed', () => {
+	const guessedWords = [
+		{ guessedWord: 'train', letterMatchCount: 3 },
+		{ guessedWord: 'agile', letterMatchCount: 1 },
+		{ guessedWord: 'party', letterMatchCount: 5 },
+	];
+	let wrapper;
+	beforeEach(() => {
+		wrapper = setup({ guessedWords });
+	});
+	test('renders without error', () => {
+		const component = findByTestAttr(wrapper, 'component-guessed-words');
+		expect(component.length).toBe(1);
+	});
+
+	test('renders "guessed words" section', () => {
+		const guessedWordsNode = findByTestAttr(wrapper, 'guessed-word');
+		expect(guessedWordsNode.length).toBe(3);
+	});
+	test('correct number of guessed words', () => {
+		const guessedWordsNodes = findByTestAttr(wrapper, 'guessed-word');
+		expect(guessedWordsNodes.length).toBe(guessedWords.length);
+	});
+});
