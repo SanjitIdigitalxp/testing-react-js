7b9a4f057ec7d0072d48030d9d84a81a2bac0dc1
sanjit.maji@idigitalxp.com <sanjit.maji@idigitalxp.com>
2021-07-15 08:56:20 +0530

    add helper function for letter matched between guess and secret word

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Input({ secretWord }) {
-    const [currentGuess, setCurrentGuess]  = React.useState('')
-	return <div data-test='component-input'>
-        <form className='form-inline'>
-            <input data-test='input-box' className="mb-2 mx-sm-3" type='text' placeholder='enter guess' value={currentGuess} onChange={(event)=>setCurrentGuess(event.target.value)}></input>
-            <button data-test='submit-button' className='btn btn-primary mb-2'>Submit</button>
-        </form>
-    </div>;
+function Input({success, secretWord }) {
+	const [currentGuess, setCurrentGuess] = React.useState('');
+
+    if(success){
+        return <div data-test='component-input'/>
+    }
+
+	return (
+		<div data-test='component-input'>
+			<form className='form-inline'>
+				<input
+					data-test='input-box'
+					className='mb-2 mx-sm-3'
+					type='text'
+					placeholder='enter guess'
+					value={currentGuess}
+					onChange={(event) => setCurrentGuess(event.target.value)}></input>
+				<button
+					data-test='submit-button'
+					className='btn btn-primary mb-2'
+					onClick={(evt) => {
+                        evt.preventDefault()
+						setCurrentGuess('');
+					}}>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 }
 
 Input.propTypes = {
